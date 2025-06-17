3755ca8af0996226105b92e1c044412872e2ccc4
Soomin Song <d.song.149@gmail.com>
2025-06-17 16:43:52 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode206
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            var nodes = new ListNode([1, 2, 3, 4, 5]);
+
+            PrintNodes(nodes);
+
+            solution.ReverseList(nodes);
+            PrintNodes(nodes);
+
+            nodes = new ListNode([5, 4, 3, 2, 1]);
+            PrintNodes(nodes);
+
+
+            static void PrintNodes(ListNode head)
+            {
+                while (head != null)
+                {
+                    Console.WriteLine(head.val);
+                    head = head.next;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,61 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        if (head == null) { return null; }
+        Stack<int> nodeValues = new Stack<int>();
+        ListNode ptr = head;
+        // iterate through listnode and push into stack
+        while (ptr != null)
+        {
+            nodeValues.Push(ptr.val);
+            ptr = ptr.next;
+        }
+
+        // iterate through listnode second time and pop from stack
+        ptr = head;
+        while (ptr != null)
+        {
+            ptr.val = nodeValues.Pop();
+            ptr = ptr.next;
+        }
+
+        return head;
+    }
+}
+
+
+// Definition for singly-linked list.
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public ListNode(int[] valueArray)
+    {
+        if (valueArray == null) { return; }
+        val = valueArray[0];
+
+        ListNode ptr = this;
+        for (int i = 0; i < valueArray.Length-1; i++)
+        {
+            ptr.next = new ListNode(valueArray[i+1]);
+            ptr= ptr.next;
+        }
+        ptr.next = null;
+    }
+}
